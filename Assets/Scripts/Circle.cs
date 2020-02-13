9314e5d97e5d0e45b41dd1a8b9c730a116afae36
Blake <telefragent@gmail.com>
2020-02-13 12:16:40 -0500

    Initial commit

---
+++ Assets/Scripts/Circle.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Circle : MonoBehaviour
+{
+    public void SetPosition(float xPosition)
+	{
+		transform.position = new Vector3(xPosition, 0f, 0f);
+	}
+}
